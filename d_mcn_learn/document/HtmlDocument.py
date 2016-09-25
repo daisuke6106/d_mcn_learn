ed9fcdb9be8b239c811ec6f3575d0c6fc3c0893a
dk <daisuke6106@gmail.com>
2016-09-26 00:01:30 +0900

    docを修正

--- d_mcn_learn/document/HtmlDocument.py
+++ d_mcn_learn/document/HtmlDocument.py
@@ -13,6 +13,10 @@ class HtmlDocument(object):
     
     @staticmethod
     def getHtmlByDir(path):
+        u"""
+        引数に指定されたパスからHtmlファイルを元に複数のHtmlDocumentを生成し、返却します。
+        :return htmlList: HtmlDocumentデータリスト
+        """
         files = glob.glob(path)
         list  = []
         for file in files:
@@ -21,13 +25,27 @@ class HtmlDocument(object):
         return list
         
     def __init__(self, data):
-        '''
+        u"""
         Constructor
-        
-        :param data: Html
-        '''
+        指定のHtml本文のデータを基にHtmlDocumentのインスタンスを生成します。
+        :param data: Htmlデータ
+        """
         self.data = data
         self.bs = BeautifulSoup(data)
     
     def get_title(self):
+        u"""
+        このHtmlDocumentのtitleタグに指定されている内容を取得します。
+        :return title: このHtmlデータのタイトル文字列（文字列）
+        """
         return self.bs.find('title').text
+    
+    def get_body_content(self):
+        u"""
+        このHtmlDocumentのbodyタグに指定されている内容を取得します。
+        :return title: このHtmlデータのbody部の内容（文字列）
+        """
+        return self.bs.find('body').text
+    
+    def get_content(self, selector):
+        return self.bs.select(selector).text
