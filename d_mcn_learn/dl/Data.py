8e1ebc00c40831ed1c9666730193c73339909417
Daisuke Kanno <daisuke6106@gmail.com>
2017-02-09 01:23:22 +0900

    ソフトマックス関数に対してオーバーフロー対策を実施

--- d_mcn_learn/dl/Data.py
+++ d_mcn_learn/dl/Data.py
@@ -23,11 +23,20 @@ class Array(object):
     def sample_data(from_num = -1.0, to_num = 1.0, inc = 0.1):
         return Array(np.arange(from_num, to_num, inc))
 
+    def dot(self, array):
+        u"""
+        この行列と引数に指定された行列の積を求め、返却する。
+        """
+        return Array._dot(self.arrays, array.arrays)
+        
     @staticmethod
-    def dot(array1, array2):
+    def _dot(array1, array2):
         return Array(np.dot(array1, array2))
         
     def sigmoid(self):
+        u"""
+        この行列にシグモイド関数を実行し、その結果を返却する。
+        """
         result = Array(Array._sigmoid(self.arrays))
         return ResultArrays(self, result)
     
@@ -36,6 +45,9 @@ class Array(object):
         return 1 / (1 + np.exp(-array))
     
     def relu(self):
+        u"""
+        この行列にRelu関数を実行し、その結果を返却する。
+        """
         result = Array(Array._relu(self.arrays))
         return ResultArrays(self, result)
     
@@ -59,7 +71,10 @@ class Array(object):
         u""" 
              ソフトマックス関数の実施結果を返却する。
         """
-        exp_a = np.exp(self.arrays)
+        
+        # オーバーフロー対策の為、入力信号の中で最大値を差し引く
+        maxum = np.max(self.arrays)
+        exp_a = np.exp(self.arrays - maxum)
         sum_exp_a = np.sum(exp_a)
         y = exp_a / sum_exp_a
         return y
